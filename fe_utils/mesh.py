558e9c1315cc2036d4af10229a2981f893a4bfb0
Jiaao Wang <jw2020@ic.ac.uk>
2021-03-11 10:36:28 +0000

    Revised jacobian.

--- fe_utils/mesh.py
+++ fe_utils/mesh.py
@@ -123,7 +123,7 @@ class Mesh(object):
         vertex_coords = self.vertex_coords[self.cg1fs.cell_nodes[c, :], :]
 
         # evaluate the Jacobian at the cell origin X=0
-        result = np.dot(self.coord_map[0].T, vertex_coords)
+        result = np.dot(self.coord_map[0].T, vertex_coords).T
         
         return result
 
